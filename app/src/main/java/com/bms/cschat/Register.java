f546fa8434a9f43132e541f615144a17b60327b1
mani9technologies <96204229+mani9technologies@users.noreply.github.com>
2022-08-01 23:16:18 +0100

    Animation done and Responsiveness done

--- app/src/main/java/com/bms/cschat/Register.java
+++ app/src/main/java/com/bms/cschat/Register.java
@@ -3,58 +3,34 @@ package com.bms.cschat;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-
-import java.sql.*;
+import android.widget.ProgressBar;
 
 public class Register extends AppCompatActivity {
     ConstraintLayout toLogin;
     EditText nickname,email,password;
     Button registerButton;
+    ProgressBar progressBar;
 
-    TextView t1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        
+        //Progress Bar
+        progressBar  = findViewById(R.id.progressBarRegister);
+
+        // Buttons
+        toLogin = findViewById(R.id.toLogin);
+        registerButton = findViewById(R.id.RegisterButton);
+
+        //EditTexts
         nickname = findViewById(R.id.EditTextRegisterNickname);
         email = findViewById(R.id.EditTextRegisterEmail);
         password = findViewById(R.id.EditTextRegisterPassword);
-        registerButton = findViewById(R.id.RegisterButton);
-        t1 = findViewById(R.id.t1);
-        String nicknametxt = nickname.getText().toString();
-        String emailtxt = email.getText().toString();
-        String passwordtxt = password.getText().toString();
 
-        registerButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                try {
-                    Connection cn = DriverManager.getConnection("127.0.0.1","root","");
-                    Statement stmt = cn.createStatement();
-                    String insert = "INSERT INTO `users` (`id`, `nickname`, `email`, `password`) VALUES ('1', 'nicknametxt', 'emailtxt', 'passwordtxt');";
-                    ResultSet rst = stmt.executeQuery(insert);
-                } catch (SQLException throwables) {
-                    t1.setText("Error");
-                }
-            }
-        });
-
-
-
-        toLogin = findViewById(R.id.toLogin);
-        toLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(getApplicationContext(),Login.class);
-                startActivity(i);
-            }
-        });
     }
 }
