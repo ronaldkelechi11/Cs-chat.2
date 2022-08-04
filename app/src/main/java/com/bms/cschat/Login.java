54d4df3ed1831fcfeb6b0def2ed954ebc80b6336
mani9technologies <96204229+mani9technologies@users.noreply.github.com>
2022-08-04 22:55:25 +0100

    Some front end validation in the login page

--- app/src/main/java/com/bms/cschat/Login.java
+++ app/src/main/java/com/bms/cschat/Login.java
@@ -5,12 +5,17 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Timer;
 
 public class Login extends AppCompatActivity{
     ConstraintLayout toRegister;
@@ -37,6 +42,51 @@ public class Login extends AppCompatActivity{
 
         //Firebase
         mAuth = FirebaseAuth.getInstance();
+
+
+        // Button Functionality for To Register click
+        toRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent t = new Intent(getApplicationContext(),Register.class);
+                startActivity(t);
+            }
+        });
+
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String emailSt = email.getText().toString();
+                String passwordSt = password.getText().toString();
+
+                //Front-End Validation
+                if(emailSt.isEmpty()){
+                    email.setError("Email cannot be empty");
+                    email.requestFocus();
+                    return;
+                }
+                if(passwordSt.isEmpty()){
+                    password.setError("Password cannot be empty");
+                    password.requestFocus();
+                    return;
+                }
+                if(!Patterns.EMAIL_ADDRESS.matcher(emailSt).matches()){
+                    email.setError("Not a valid Email Address");
+                    email.requestFocus();
+                    return;
+                }
+                if(passwordSt.length() < 6){
+                    password.setError("Invalid Password");
+                    password.requestFocus();
+                    return;
+                }
+
+
+
+
+            }
+        });
 
     }
 }
