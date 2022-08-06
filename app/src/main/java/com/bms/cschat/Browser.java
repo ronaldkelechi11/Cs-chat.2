465a51913f126ae2ea6fce782fb1f37cb0c83d81
mani9technologies <96204229+ronaldkelechi11@users.noreply.github.com>
2022-08-06 18:29:13 +0100

    Home Screen click Set Up and Browser except to setup the Home Page

--- app/src/main/java/com/bms/cschat/Browser.java
+++ app/src/main/java/com/bms/cschat/Browser.java
@@ -2,13 +2,111 @@ package com.bms.cschat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class Browser extends AppCompatActivity {
+    EditText browserSearch;
+    WebView webView;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_browser);
+
+        webView = findViewById(R.id.browserWebview);
+        browserSearch = findViewById(R.id.browser_search);
+        progressBar = findViewById(R.id.progressBarWebView);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setBuiltInZoomControls(true);
+        webSettings.setDisplayZoomControls(false);
+
+        webView.setWebViewClient(new MyWebViewClient());
+        //Set the default Homepage here
+        //webView.loadUrl("www. .com");
+
+        browserSearch.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                if(i == EditorInfo.IME_ACTION_GO || i == EditorInfo.IME_ACTION_DONE){
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(browserSearch.getWindowToken(),0);
+                    loadUrl(browserSearch.getText().toString());
+                }
+                else {
+
+                }
+                return false;
+            }
+        });
+
     }
+
+    //Back Pressed Button
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()){
+            webView.goBack();
+        }
+        else{
+            Intent i = new Intent(getApplicationContext(),HomeScreen.class);
+            startActivity(i);
+        }
+        super.onBackPressed();
+    }
+
+
+    // Method to load URL and check if it's a url
+    void loadUrl(String url){
+        boolean isUrl = Patterns.WEB_URL.matcher(url).matches();
+        if(isUrl){
+          webView.loadUrl(url);
+        }
+        else{
+           webView.loadUrl("google.com/search?q"+url);
+        }
+    }
+
+
+    // Web Client Service
+    class MyWebViewClient extends WebViewClient {
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            return super.shouldOverrideUrlLoading(view, request);
+        }
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+            progressBar.setVisibility(View.VISIBLE);
+
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            progressBar.setVisibility(View.INVISIBLE);
+        }
+    }
+
+
+
 }
--- app/src/main/java/com/bms/cschat/HomeScreen.java
+++ app/src/main/java/com/bms/cschat/HomeScreen.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class HomeScreen extends AppCompatActivity implements View.OnClickListener {
     @Override
@@ -22,9 +24,31 @@ public class HomeScreen extends AppCompatActivity implements View.OnClickListene
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-
-
-
+            case R.id.boardButton:
+                Toast.makeText(getApplicationContext(), "Board is Locked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.timetableButton:
+                Toast.makeText(getApplicationContext(), "Timetable is Locked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.notesButton:
+                Toast.makeText(getApplicationContext(), "Notes is Locked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.tradeButton:
+                Toast.makeText(getApplicationContext(), "Trade is Locked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.ebookButton:
+                Toast.makeText(getApplicationContext(), "E-books is Locked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.browserButton:
+                Intent i = new Intent(getApplicationContext(),Browser.class);
+                startActivity(i);
+                break;
+            case R.id.reminderButton:
+                Toast.makeText(getApplicationContext(), "Reminders is Locked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.moreButton:
+                Toast.makeText(getApplicationContext(), "Coming Soon", Toast.LENGTH_SHORT).show();
+                break;
         }
     }
 
