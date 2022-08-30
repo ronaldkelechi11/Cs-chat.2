ec77e55a51bbd5c7bc4c61c2c68b454c3101eaf8
mani9technologies <96204229+ronaldkelechi11@users.noreply.github.com>
2022-08-30 13:59:55 +0100

    New changes to UI starting from MainScreen and Animation

--- app/src/main/java/com/bms/cschat/MainActivity.java
+++ app/src/main/java/com/bms/cschat/MainActivity.java
@@ -6,10 +6,10 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
-    int splashscreentime = 2000;
 
     private static final String SHARED_PREFERNCE_NAME = "mySharedPreferences";
     SharedPreferences myPref;
@@ -20,38 +20,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Go to FullScreen
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
 
+    public void goToLogin(View view) {
+        Intent l = new Intent(getApplicationContext(), Login.class);
+        startActivity(l);
+    }
 
-myPref = getSharedPreferences(SHARED_PREFERNCE_NAME,MODE_PRIVATE);
-String IS_LOGGED_IN = myPref.getString(KEY,"default");
-
-        // Increase Splashscreen delay time by increasing the variable above
-        if(IS_LOGGED_IN.equals("true")){
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    Intent t = new Intent(getApplicationContext(),HomeScreen.class);
-                    startActivity(t);
-                    finish();
-                }
-            },splashscreentime);
-        }
-        else{
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    Intent i = new Intent(getApplicationContext(),Register.class);
-                    startActivity(i);
-                    finish();
-                }
-            },splashscreentime);
-        }
-
-
-
-
+    public void gotoRegister(View view) {
+        Intent i = new Intent(getApplicationContext(), Register.class);
+        startActivity(i);
     }
 }
