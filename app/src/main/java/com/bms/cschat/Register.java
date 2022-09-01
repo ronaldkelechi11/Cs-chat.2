06f35facdaf9b07a20e9f2d4dad1d09106ee6e76
mani9technologies <96204229+ronaldkelechi11@users.noreply.github.com>
2022-09-01 11:02:40 +0100

    Getting Warmed Up 😁

--- app/src/main/java/com/bms/cschat/Register.java
+++ app/src/main/java/com/bms/cschat/Register.java
@@ -2,10 +2,15 @@ package com.bms.cschat;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.renderscript.ScriptGroup;
+import android.telephony.PhoneNumberFormattingTextWatcher;
+import android.text.Editable;
 import android.text.InputType;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -30,19 +35,27 @@ public class Register extends AppCompatActivity {
         passwordToggle1 = findViewById(R.id.password_toggle1);
         passwordToggle2 = findViewById(R.id.password_toggle2);
 
+
+
+
+        // To format the code the number to be Nigerian Number Format
+        numberEditText.addTextChangedListener(new PhoneNumberFormattingTextWatcher("NG"));
+
         // First check the type that is there then changes it to it's opposite
         passwordToggle1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-              boolean isPasswordType = passwordEditText.getInputType() == InputType.TYPE_TEXT_VARIATION_PASSWORD;
-              if(isPasswordType){
-                  passwordEditText.setInputType(InputType.TYPE_CLASS_TEXT);
-                  passwordToggle1.setImageResource(R.drawable.closed_eye);
-              }
-              if(!isPasswordType){
-                  passwordEditText.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD);
-                  passwordToggle1.setImageResource(R.drawable.open_eye);
-              }
+             boolean isPassword = passwordEditText.getInputType() == InputType.TYPE_TEXT_VARIATION_PASSWORD;
+                if (isPassword) {
+                    Toast.makeText(getApplicationContext(), "Password is Hidden", Toast.LENGTH_SHORT).show();
+                    passwordEditText.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                    passwordToggle1.setImageResource(R.drawable.open_eye);
+                }
+                if(!isPassword){
+                    Toast.makeText(getApplicationContext(), "Password is not Hidden", Toast.LENGTH_SHORT).show();
+                    passwordEditText.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    passwordToggle1.setImageResource(R.drawable.closed_eye);
+                }
             }
         });
 
@@ -51,5 +64,9 @@ public class Register extends AppCompatActivity {
     public void goToMain(View view) {
         Intent i = new Intent(getApplicationContext(),MainActivity.class);
         startActivity(i);
+    }
+
+    public void goToHomeScreen(View view) {
+        Toast.makeText(getApplicationContext(), "You clicked Sign Up", Toast.LENGTH_SHORT).show();
     }
 }
