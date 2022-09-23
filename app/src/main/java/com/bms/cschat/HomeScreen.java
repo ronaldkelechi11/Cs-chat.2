9b4863d5e92680a7d802dca70a61f0068cfa8421
mani9technologies <96204229+ronaldkelechi11@users.noreply.github.com>
2022-09-23 14:39:59 +0100

    Finished the hide and show features. Next Shared Prefernces

--- app/src/main/java/com/bms/cschat/HomeScreen.java
+++ app/src/main/java/com/bms/cschat/HomeScreen.java
@@ -8,11 +8,13 @@ import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -31,16 +33,38 @@ import java.util.ArrayList;
 
 
 public class HomeScreen extends AppCompatActivity{
+    TextView seeMore;
+    TableRow thirdrow;
+    CardView crgButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
 
+        //Field Initialization
+        seeMore = findViewById(R.id.seeMore);
+        thirdrow = findViewById(R.id.thirdRow);
+        crgButton = findViewById(R.id.readingGroupCreate);
+
+
     }//End of Initial Class
 
     public void goToNotes(View view) {
         Intent i = new Intent(getApplicationContext(),Notes.class);
         startActivity(i);
+    }
+
+    public void changeVisi(View view) {
+        if(seeMore.getText().toString() == "See more"){
+            seeMore.setText("See less");
+            thirdrow.setVisibility(View.VISIBLE);
+            crgButton.setVisibility(View.GONE);
+        }
+        else{
+            seeMore.setText("See more");
+            thirdrow.setVisibility(View.GONE);
+            crgButton.setVisibility(View.VISIBLE);
+        }
     }
 }
